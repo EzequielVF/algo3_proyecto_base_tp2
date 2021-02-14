36b74fcf58570a607aa3262e9f0e08a731ecf949
ignacioLosc <iloscocco@fi.uba.ar>
2021-02-14 18:05:11 -0300

    Mejoro el nombre de varias variables y agrego mas pruebas para accionTest

--- src/main/test/edu/fiuba/algo3/modelo/AccionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/AccionTest.java
@@ -49,23 +49,62 @@ public class AccionTest {
     public void PruebaBajarPincelCambiaCorrectamenteElEstadoDelPincel() {
         Personaje personaje = new Personaje();
         Accion accion = new BajarPincel();
-        EstadoPincel aux = new PincelAbajo();
-        EstadoPincel auxArriba = new PincelArriba();
-        assertEquals(auxArriba.getClass(),personaje.devolverEstadoPincel().getClass());
+        EstadoPincel pincelAbajo = new PincelAbajo();
+        EstadoPincel pincelArriba = new PincelArriba();
+        assertEquals(pincelArriba.getClass(),personaje.devolverEstadoPincel().getClass());
         accion.aplicarAccion(personaje);
-        assertEquals(aux.getClass(),personaje.devolverEstadoPincel().getClass());
+        assertEquals(pincelAbajo.getClass(),personaje.devolverEstadoPincel().getClass());
     }
     @Test
     public void PruebaSubirPincelCambiaCorrectamenteElEstadoDelPincel() {
         Personaje personaje = new Personaje();
         Accion accion = new BajarPincel();
         Accion subir = new SubirPincel();
-        EstadoPincel aux = new PincelAbajo();
-        EstadoPincel auxArriba = new PincelArriba();
-        assertEquals(auxArriba.getClass(),personaje.devolverEstadoPincel().getClass());
+        EstadoPincel pincelAbajo = new PincelAbajo();
+        EstadoPincel pincelArriba = new PincelArriba();
+        assertEquals(pincelArriba.getClass(),personaje.devolverEstadoPincel().getClass());
         accion.aplicarAccion(personaje);
-        assertEquals(aux.getClass(),personaje.devolverEstadoPincel().getClass());
+        assertEquals(pincelAbajo.getClass(),personaje.devolverEstadoPincel().getClass());
         subir.aplicarAccion(personaje);
-        assertEquals(auxArriba.getClass(),personaje.devolverEstadoPincel().getClass());
+        assertEquals(pincelArriba.getClass(),personaje.devolverEstadoPincel().getClass());
+    }
+    @Test
+    public void PruebaRepetirPorDosModificaCorrectamenteLaPosicionDelPersonaje() {
+        Personaje personaje = new Personaje();
+        Accion accion = new MoverALaDerecha();
+        Posicion posicion = new Posicion();
+        assertEquals(posicion.hashCode(),personaje.posicionActual());
+        RepetirPorDos repetirPorDos = new RepetirPorDos();
+        repetirPorDos.almacenarAccion(new MoverALaDerecha());
+        repetirPorDos.aplicarAccion(personaje);
+        posicion.sumarAColumna();
+        posicion.sumarAColumna();
+        assertEquals(posicion.hashCode(), personaje.posicionActual());
+    }
+    @Test
+    public void PruebaRepetirPorTresModificaCorrectamenteLaPosicionDelPersonaje() {
+        Personaje personaje = new Personaje();
+        Accion accion = new MoverALaDerecha();
+        Posicion posicion = new Posicion();
+        assertEquals(posicion.hashCode(),personaje.posicionActual());
+        RepetirPorTres repetirPorTres = new RepetirPorTres();
+        repetirPorTres.almacenarAccion(new MoverALaDerecha());
+        repetirPorTres.aplicarAccion(personaje);
+        posicion.sumarAColumna();
+        posicion.sumarAColumna();
+        posicion.sumarAColumna();
+        assertEquals(posicion.hashCode(), personaje.posicionActual());
+    }
+    @Test
+    public void PruebaInvertirComportamientoModificaCorrectamenteLaPosicionDelPersonaje() {
+        Personaje personaje = new Personaje();
+        Accion accion = new MoverALaDerecha();
+        Posicion posicion = new Posicion();
+        assertEquals(posicion.hashCode(),personaje.posicionActual());
+        InvertirComportamiento invertirComportamiento = new InvertirComportamiento();
+        invertirComportamiento.almacenarAccion(new MoverALaDerecha());
+        invertirComportamiento.aplicarAccion(personaje);
+        posicion.restarAColumna();
+        assertEquals(posicion.hashCode(), personaje.posicionActual());
     }
 }
--- src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
@@ -8,31 +8,31 @@ public class PosicionTest {
     @Test
     public void PosicionSeIniciaEnLaPosicionCorrecta() {
         Posicion posicion = new Posicion();
-        assertEquals(3,posicion.columna);
-        assertEquals(3,posicion.fila);
+        assertEquals(3,posicion.devolverColumna());
+        assertEquals(3,posicion.devolverFila());
     }
     @Test
     public void PosicionSumarColumnaActualizaCorrectamente() {
         Posicion posicion = new Posicion();
         posicion.sumarAColumna();
-        assertEquals(4,posicion.columna);
+        assertEquals(4,posicion.devolverColumna());
     }
     @Test
     public void PosicionRestarColumnaActualizaCorrectamente() {
         Posicion posicion = new Posicion();
         posicion.restarAColumna();
-        assertEquals(2,posicion.columna);
+        assertEquals(2,posicion.devolverColumna());
     }
     @Test
     public void PosicionSumarFilaActualizaCorrectamente() {
         Posicion posicion = new Posicion();
         posicion.sumarAFila();
-        assertEquals(4,posicion.fila);
+        assertEquals(4,posicion.devolverFila());
     }
     @Test
     public void PosicionRestarFilaActualizaCorrectamente() {
         Posicion posicion = new Posicion();
         posicion.restarAFila();
-        assertEquals(2,posicion.fila);
+        assertEquals(2,posicion.devolverFila());
     }
 }
